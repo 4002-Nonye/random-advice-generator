b6613ad82bb3634d9e5ddcd43729b0e44d6bd266
4002-Nonye <105207196+4002-Nonye@users.noreply.github.com>
2023-02-09 10:08:16 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import React from "react";
 import "./components/css/index.css";
 import AdviceGenerator from "./components/js/AdviceGenerator";
 
-
 const App = () => {
   return (
     <div>
-      {" "}
       <AdviceGenerator />
-   
     </div>
   );
 };
 
 export default App;
-
-// const App = () => {
-//     const isLoading = false;
-//       if (isLoading) {
-//         return <FadeLoader />;
-//       }
-//       return <h1 style={{backgroundColor: "lightblue"}}>it is not loading</h1>;
-//     };
